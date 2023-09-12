f179541fd46904b6744ddd5cd11fba685c16e0db
Stijn121 <s.engelmoer@ziggo.nl>
2023-09-12 09:48:43 +0200

    #16 gedaan

--- cart.service.php
+++
@@ -1,6 +0,0 @@
-<?php
-include "carts.repository.php";
-function SearchForTop5Products(){
-    $carts = GetTop5Products();
-    return $carts;
-}
--- home.php
+++
@@ -1,5 +0,0 @@
-<?php 
-function ShowHomeContent() { echo '
-<p>Welkom bij de website.</p>
-<p>We hopen dat u ervan geniet.</p>
-';}
